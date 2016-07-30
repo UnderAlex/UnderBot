a5c124fd680e130452f87b2c55391a96194f5408
arhiwarius <arhiwarius@ya.ru>
2016-07-31 01:18:18 +0300

    +1

--- app.js
+++ app.js
@@ -2,41 +2,58 @@
 
 var TelegramBot = require('node-telegram-bot-api');
 
-var TOKEN = '241137685:AAGz5moUy-Wy7fooqRLUffjs7osFozpJ3zs';
+var TelegramBot = require('../src/telegram');
+var request = require('request');
 
-var port = process.env.OPENSHIFT_NODEJS_PORT;
-var host = process.env.OPENSHIFT_NODEJS_IP;
-var domain = process.env.OPENSHIFT_APP_DNS;
+var options = {
+  polling: true
+};
 
-var bot = new TelegramBot(TOKEN, {webHook: {port: port, host: host}});
-// OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
-bot.setWebHook(domain+':443/bot'+TOKEN);
-bot.on('message', function (msg) {
-  var chatId = msg.chat.id;
-  bot.sendMessage(chatId, "I'm alive!");
+var token = process.env.TELEGRAM_BOT_TOKEN || 'Y241137685:AAGz5moUy-Wy7fooqRLUffjs7osFozpJ3zs';
+
+var bot = new TelegramBot(token, options);
+bot.getMe().then(function (me) {
+  console.log('Hi my name is %s!', me.username);
 });
 
+// Matches /photo
+bot.onText(/\/photo/, function (msg) {
+  var chatId = msg.chat.id;
+  // From file
+  var photo = __dirname+'/../test/bot.gif';
+  bot.sendPhoto(chatId, photo, {caption: "I'm a bot!"});
+});
 
+// Matches /audio
+bot.onText(/\/audio/, function (msg) {
+  var chatId = msg.chat.id;
+  var url = 'https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg';
+    // From HTTP request!
+    var audio = request(url);
+    bot.sendAudio(chatId, audio)
+      .then(function (resp) {
+        // Forward the msg
+        var messageId = resp.message_id;
+        bot.forwardMessage(chatId, chatId, messageId);
+      });
+});
 
-/**
+// Matches /love
+bot.onText(/\/love/, function (msg) {
+  var chatId = msg.chat.id;
+  var opts = {
+      reply_to_message_id: msg.message_id,
+      reply_markup: JSON.stringify({
+        keyboard: [
+          ['Yes, you are the bot of my life ?'],
+          ['No, sorry there is another one...']]
+      })
+    };
+    bot.sendMessage(chatId, 'Do you love me?', opts);
+});
 
-const Telegram = require('node-telegram-bot-api')
-const TelegramBaseController = Telegram.TelegramBaseController
-const tg = new Telegram.Telegram('241137685:AAGz5moUy-Wy7fooqRLUffjs7osFozpJ3zs')
-
-class PingController extends TelegramBaseController {
-
-    pingHandler($) {
-        $.sendMessage('pong')
-    }
-
-    get routes() {
-        return {
-            'ping': 'pingHandler'
-        }
-    }
-}
-
-tg.router
-    .when(['ping'], new PingController())
-**/
+bot.onText(/\/echo (.+)/, function (msg, match) {
+  var chatId = msg.chat.id;
+  var resp = match[1];
+  bot.sendMessage(chatId, resp);
+});
